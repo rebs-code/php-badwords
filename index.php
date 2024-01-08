90b7b030c70dc6a1cfcb91ff73ff9da73d5200d4
rebs-code <jack.rebussi@gmail.com>
2024-01-08 17:28:15 +0100

    add: form tag and method + action

--- index.php
+++ index.php
@@ -15,11 +15,14 @@
         <h1>Free Censorship</h1>
     </header>
     <div class="container-fluid">
-        <div class="mb-3 w-25 mx-auto mt-5">
-            <label for="exampleFormControlTextarea1" class="form-label">Insert the text you'd like to censor</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
-            <button type="submit" class="mt-3">Submit</button>
-        </div>
+        <form action="censor.php" method="post">
+            <div class="mb-3 w-25 mx-auto mt-5">
+                <label for="exampleFormControlTextarea1" class="form-label">Insert the text you'd like to censor</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                <button type="submit" class="mt-3">Submit</button>
+            </div>
+        </form>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
